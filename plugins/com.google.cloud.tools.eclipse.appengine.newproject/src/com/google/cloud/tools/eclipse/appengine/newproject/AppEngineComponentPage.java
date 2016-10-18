1b1f970e6bf9ae7416e19b8cdc87ea55cd4b116a
nbashirbello <nbashirbello@google.com>
2016-10-18 14:16:20 -0400

    nit

--- plugins/com.google.cloud.tools.eclipse.appengine.newproject/src/com/google/cloud/tools/eclipse/appengine/newproject/AppEngineComponentPage.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.newproject/src/com/google/cloud/tools/eclipse/appengine/newproject/AppEngineComponentPage.java
@@ -17,22 +17,6 @@
 package com.google.cloud.tools.eclipse.appengine.newproject;
 
 import org.eclipse.jface.dialogs.Dialog;
-/*
- * Copyright 2016 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
@@ -43,6 +27,10 @@ import org.eclipse.swt.widgets.Text;
 import com.google.cloud.tools.eclipse.usagetracker.AnalyticsEvents;
 import com.google.cloud.tools.eclipse.usagetracker.AnalyticsPingManager;
 
+/**
+ * AppEngineComponentPage is a page that displays a message that Gcloud App Engine Java component is missing
+ * with instructions on how to install it. This page disables the 'Finish' button.
+ */
 public class AppEngineComponentPage extends WizardPage {
 
   protected AppEngineComponentPage() {
