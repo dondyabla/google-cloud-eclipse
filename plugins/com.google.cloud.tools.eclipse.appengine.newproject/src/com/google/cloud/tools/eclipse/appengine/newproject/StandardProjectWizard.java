bb0497ffac18e9ff00e37a660c46a01f453c0ae9
nbashirbello <nbashirbello@google.com>
2016-10-21 11:52:38 -0400

    Minor changes

--- plugins/com.google.cloud.tools.eclipse.appengine.newproject/src/com/google/cloud/tools/eclipse/appengine/newproject/StandardProjectWizard.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.newproject/src/com/google/cloud/tools/eclipse/appengine/newproject/StandardProjectWizard.java
@@ -29,7 +29,6 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
-import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.ide.undo.WorkspaceUndoUtil;
@@ -40,22 +39,22 @@ import java.lang.reflect.InvocationTargetException;
 
 public class StandardProjectWizard extends Wizard implements INewWizard {
 
-  private WizardPage page;
+  private AppEngineStandardWizardPage page;
   private AppEngineStandardProjectConfig config = new AppEngineStandardProjectConfig();
 
   public StandardProjectWizard() {
     this.setWindowTitle("New App Engine Standard Project");
-    if (doesAppEngineJavaComponentExist()) {
-      page = new AppEngineStandardWizardPage();
-    } else {
-      page = new AppEngineComponentPage();
-    }
     setNeedsProgressMonitor(true);
   }
 
   @Override
   public void addPages() {
-    this.addPage(page);
+    if (doesAppEngineJavaComponentExist()) {
+      page = new AppEngineStandardWizardPage();
+      this.addPage(page);
+    } else {
+      this.addPage(new AppEngineComponentPage());
+    }
   }
 
   @Override
@@ -73,14 +72,13 @@ public class StandardProjectWizard extends Wizard implements INewWizard {
       config.setCloudSdkLocation(location);
     }
 
-    AppEngineStandardWizardPage createProjectPage = (AppEngineStandardWizardPage) page;
-    config.setPackageName(createProjectPage.getPackageName());
-    config.setProject(createProjectPage.getProjectHandle());
-    if (!createProjectPage.useDefaults()) {
-      config.setEclipseProjectLocationUri(createProjectPage.getLocationURI());
+    config.setPackageName(page.getPackageName());
+    config.setProject(page.getProjectHandle());
+    if (!page.useDefaults()) {
+      config.setEclipseProjectLocationUri(page.getLocationURI());
     }
 
-    config.setAppEngineLibraries(createProjectPage.getSelectedLibraries());
+    config.setAppEngineLibraries(page.getSelectedLibraries());
 
     // todo set up
     final IAdaptable uiInfoAdapter = WorkspaceUndoUtil.getUIInfoAdapter(getShell());
